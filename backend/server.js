92d56303396202501359df774cc5833c03276143
OM192004 <ommali2672@gmail.com>
2024-12-28 20:29:55 +0530

    login & registration done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
 const dotenv=require("dotenv");
+const userRoutes = require('./routes/authRoutes');
 
 const app=express();
 
@@ -9,6 +10,8 @@ const PORT=5000;
 
 app.use(cors());
 dotenv.config();
+app.use(express.json()); // Parses incoming JSON requests
+
 
 const MONGO_URL=process.env.MONGO_URL;
 
@@ -18,6 +21,9 @@ mongoose.connect(MONGO_URL)
     })
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
+
+//app.use('/users', authRoutes);
+app.use('/users', userRoutes);
 
 app.listen(PORT,()=>{
     console.log("app listenng on port:",PORT);
